1780a15c3772e02933a0739ab63aaaf8dfa134f7
ankitnandwani <ankit.nandwani@dpworld.com>
2025-03-26 00:57:33 +0530

    build service

--- src/main/java/ticket/booking/services/UserBookingService.java
+++ src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,10 +1,20 @@
 package ticket.booking.services;
 
+import com.google.gson.Gson;
 import ticket.booking.entities.User;
 
+import java.io.File;
+import java.util.List;
+
 public class UserBookingService {
+
+    private static final String USERS_PATH="../localDb/users.json";
+
+    private Gson gson = new Gson();
     private User user;
+    private List<User> userList;
     public UserBookingService(User user){
         this.user=user;
+        File users = new File(USERS_PATH);
     }
 }
