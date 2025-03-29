41555495239952884feaf4779c066d3ecb336ea1
ankitnandwani <ankit.nandwani@dpworld.com>
2025-03-30 02:17:46 +0530

    signup

--- src/main/java/ticket/booking/services/UserBookingService.java
+++ src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,13 +2,16 @@ package ticket.booking.services;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import ticket.booking.entities.Ticket;
 import ticket.booking.entities.User;
 import ticket.booking.utils.UserServiceUtil;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,19 +35,35 @@ public class UserBookingService {
     }
 
     public Boolean loginUser(){
-        Optional<User> foundUser = userList.stream().filter(user1 -> {
-            return user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashPassword());
-        }).findFirst();
+        Optional<User> foundUser = userList.stream().filter(user1 -> user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashPassword())).findFirst();
         return foundUser.isPresent();
     }
 
-//    public Boolean signUp(User user1){
-//        try {
-//            userList.add(user1);
-//            saveUserListToFile();
-//            return Boolean.TRUE;
-//        } catch (IOException ex){
-//            return Boolean.FALSE;
-//        }
-//    }
+    public Boolean signUp(User user1){
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException{
+        File usersFile = new File(USERS_PATH);
+        try (FileWriter writer = new FileWriter(usersFile)){
+            gson.toJson(userList, writer);
+        }
+    }
+
+    public void fetchBooking(){
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId) throws IOException {
+        List<Ticket> tickets = user.getTicketsBooked();
+        tickets.remove(user.getTicketsBooked().stream().filter(e->e.getTicketId().equals(ticketId)).findFirst().get());
+        saveUserListToFile();
+        return true;
+    }
 }
